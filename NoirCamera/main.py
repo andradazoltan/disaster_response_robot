7d67415379326ceb33767e062e992b6b67016b1b
gracez72 <gracezhang72@gmail.com>
2018-03-14 21:22:31 -0700

    tested landmark and box, added more user options

--- NoirCamera/main.py
+++ NoirCamera/main.py
@@ -9,15 +9,24 @@ import cv2
 import sys
 import dlib 
 import argparse
+import numpy as np
+
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+topLeft                = (25,25)
+fontScale              = 0.5
+fontColor              = (255,255,255)
+lineType               = 2
+
 
 """
 Finds face with webcam
 
-@param arg.l: include landmarks?
-@param arg.s: save faces found?
-@param arg.f: compare with given face?
+@param l: include landmarks?
+@param s: save faces found?
+@param f: compare with given face?
+@param v: verbose?
 """
-def findFace(arg.l, arg.s, args.f):
+def findFace(l, s, f, v):
 
     #Create HOG face detector from dlib class
     face_detector = dlib.get_frontal_face_detector()
@@ -36,26 +45,38 @@ def findFace(arg.l, arg.s, args.f):
         
         detected_faces = face_detector(gray, 1)
         
-        print("Found {} faces in this frame.".format(len(detected_faces)))
+        if (v): print("Found {} faces in this frame.".format(len(detected_faces)))
         
         # Loop through all faces found
         for i, face_rect in enumerate(detected_faces):
-            print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
+            if (v): print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
 
             # Draw a box around each face we found
             cv2.rectangle(frame, (face_rect.left(), face_rect.top()), (face_rect.right(), face_rect.bottom()), 
                             (0, 255, 0), 2)
             crop = frame[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]
-            if (arg.s):
-                cv2.imwrite("crop.jpg", crop)
-                
-            if (arg.l):
-                face_landmarks = face_pose_predictor(frame, face_rect)
-                frame = show_landmarks(frame, face_landmarks)
-        
-        
-        #cv2.putText(frame, "FIND DA FACE".format(alpha),
-        #    (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+            
+            #Generate and store landmarks 
+            face_landmarks = face_pose_predictor(frame, face_rect)
+            points = store_landmarks(face_landmarks)
+            
+            #If user wants to save faces, save into file
+            #TODO: given filename save
+            if (s):
+                cv2.imwrite("face.jpg", crop)
+            
+            #If user wants to show landmarks on screen
+            if (l):
+                frame = show_landmarks(frame, face_landmarks, points, v)
+   
+        #Display info 
+        cv2.putText(frame,'SEARCHING FOR FACES...', 
+            topLeft, 
+            font, 
+            fontScale,
+            fontColor,
+            lineType)        
+            
         cv2.imshow('Video', frame)
             
         
@@ -73,24 +94,42 @@ Overlays facial landmarks for given face over given frame.
 @param face_landmarks: location of landmarks for detected face
 @return frame: original frame with landmarks overlayed ontop
 """    
-def show_landmarks(frame, face_landmarks):
+def show_landmarks(frame, face_landmarks, points, v):
+
     frame = frame.copy()
-    for i, point in enumerate(face_landmarks):
-        pos = (point[0, 0], point[0, 1])
-        cv2.putText(frame, str(i), pos,
-                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-                    fontScale=0.4,
-                    color=(0, 0, 255))
-        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    
+    for (x,y) in points:
+        pos = (x,y)
+        
+        if (v):
+            cv2.putText(frame, str(x) + " " + str(y), pos,
+                        fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                        fontScale=0.2,
+                        color=(0, 0, 0))
+        cv2.circle(frame, pos, 3, color=(0, 75, 255))
     return frame
+
+"""
+Converts landmarks into array of tuples with (x,y)
+
+@param frame: face_landmarks
+@return points array withs (x,y) tuples
+"""    
+def store_landmarks(face_landmarks):
+    points = np.zeros((68,2), dtype=int)
+    
+    for i in range (0,68):
+        points[i] = (face_landmarks.part(i).x, face_landmarks.part(i).y)
+    return points 
     
 """
 Determines if face "matches" face found in given image
 
-@param fileName: name of file with given image to compare to
-@param ... : face found through webcam
+@param f: name of file with given image to compare to
+@param image : face found through webcam
 """    
-def compare_to_image(args.f):
+def compare_to_image(f, image):
+    return 
 
         
 # Function commands/help/options
@@ -98,11 +137,13 @@ def main():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument("-h", "-help",action= "help", default=argparse.SUPPRESS, 
                         help = "Function identifies faces found with webcam; will locate 68 landmarks and bounding box.")
-    parser.add_argument("-l", "-landmarks", help = "Add filename with data to be parsed and graphed.", type=bool, required=False)
+    parser.add_argument("-l", "--landmarks", help = "Show found landmarks when face found.", action="store_true")
     parser.add_argument("-s", "-save", help = "Saves images of faces detected through webcam as jpg.", type=bool,required =False)
-    parser.add_argument("-f", "-file", help = "Add image to determine likelihood given face is seen through webcam.", type=str, required=false)
+    parser.add_argument("-f", "-file", help = "Add image to determine likelihood given face is seen through webcam.", type=str, required=False)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    
     args = parser.parse_args()
-    findFace(args.l, args.s, args.f)
+    findFace(args.landmarks, args.s, args.f, args.verbose)
 
 if __name__ == '__main__':
    main()
