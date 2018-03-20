69b9fe095f98e1f7150d610d2ca38c8ea55d8a27
Nancy Jiang <nancyjiang10@gmail.com>
2018-03-20 08:40:11 -0700

    added missing person form

---
+++ web/missing_person_form/formoid_files/formoid1/form.php
@@ -0,0 +1,33 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', '');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'redirect');
+define('FINISH_MESSAGE', '');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+define('_DIR_', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require_once _DIR_ . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-dark.css" type="text/css" />
+<span class="alert alert-success"><?php echo FINISH_MESSAGE; ?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-dark.css" type="text/css" />
+<script type="text/javascript" src="<?php echo dirname($form_path); ?>/jquery.min.js"></script>
+<form enctype="multipart/form-data" class="formoid-solid-dark" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Report A Missing Person</h2></div>
+	<div class="element-name<?php frmd_add_class("name"); ?>"><label class="title"></label><span class="nameFirst"><input placeholder=" First Name" type="text" size="8" name="name[first]" /><span class="icon-place"></span></span><span class="nameLast"><input placeholder=" Last Name" type="text" size="14" name="name[last]" /><span class="icon-place"></span></span></div>
+	<div class="element-number<?php frmd_add_class("number"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" min="0" max="100" name="number" placeholder="Age" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-date<?php frmd_add_class("date"); ?>"><label class="title"></label><div class="item-cont"><input class="large" data-format="yyyy-mm-dd" type="date" name="date" placeholder="Missing Since"/><span class="icon-place"></span></div></div>
+	<div class="element-phone<?php frmd_add_class("phone"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" placeholder="Call-back Number" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-file<?php frmd_add_class("file"); ?>"><label class="title"></label><div class="item-cont"><label class="large" ><div class="button">Choose File</div><input type="file" class="file_input" name="file" /><div class="file_text">No file selected</div><span class="icon-place"></span></label></div></div>
+<div class="submit"><input type="submit" value="Submit"/></div></form><script type="text/javascript" src="<?php echo dirname($form_path); ?>/formoid-solid-dark.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
