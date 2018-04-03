45158e567ccbb4feec86b59287e0c7823de13373
gracez72 <gracezhang72@gmail.com>
2018-04-02 19:05:06 -0700

    fixed left and right detection

--- MainRobot/camera.py
+++ MainRobot/camera.py
@@ -1,13 +1,8 @@
 import cv2
-import sys
-import robot
-import dlib 
+import io
 import argparse
-import imutils
-import numpy as np
-from scipy.spatial import distance 
-import math
-import time
+import sys
+import dlib
 import json
 import subprocess
 import requests
@@ -15,53 +10,85 @@ import requests
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
+robot_id = None
 URL = 'http://38.88.75.83/db/uploadfile2.php'
+global_counter = 0
 
 def start_camera():
+
     camera = PiCamera()
     stream = io.BytesIO()
-    camera.resolution = (128,128)
-    camera.framerate = 10
-    rawCapture = PiRGBArray(camera, size=(128,128))
+    camera.resolution =(608,480)
+    camera.framerate = 50
+    rawCapture = PiRGBArray(camera, size=(608,480))
 
     #Create HOG face detector from dlib class
-    #face_detector = dlib.get_frontal_face_detector()
+    face_detector = dlib.get_frontal_face_detector()
     #landmark_predictor_model = "shape_predictor_68_face_landmarks.dat"
 
     counter = 0
+    global_counter = 0
 
     for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port = True):
         frame = image.array
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        x, y = frame.shape
-        MU = cv2.getRotationMatrix2D(int(x/2), int(y/2), 180, 1)
-        upright = cv2.wrapAffine(frame, MU, (int(x/2), int(y/2)))
-
         rows, cols = gray.shape
+        MU = cv2.getRotationMatrix2D((int(cols/2), int(rows/2)), 180, 1)
+        grayup = cv2.warpAffine(gray,MU,(int(cols/2), int(rows/2)))
         ML = cv2.getRotationMatrix2D((int(cols/2), int(rows/2)),90, 1)
         grayleft = cv2.warpAffine(gray,ML,(int(cols/2), int(rows/2)))
         MR = cv2.getRotationMatrix2D((int(cols/2), int(rows/2)),270, 1)
         grayright = cv2.warpAffine(gray,MR,(int(cols/2), int(rows/2)))
-        #detected_faces = face_detector(gray,1)
-        #detected_left = face_detector(grayleft,1)
-        #detected_right = face_detector(grayright,1)
+        detected_faces = face_detector(grayup,1)
+        detected_left = face_detector(grayleft,1)
+        detected_right = face_detector(grayright,1)
+        
     
-        #if (len(detected_faces) + len(detected_left) + len(detected_right) > 0): #face detected
-        
-        if (counter == 90):
-            cv2.imwrite("frame.jpg", upright)
-            requests.post(URL, files = cv2.imread("frame.jpg"))
+        if (len(detected_faces) + len(detected_left) + len(detected_right) > 0):
+            for i, face_rect in enumerate(detected_left):
+                frameLeft = cv2.warpAffine(frame, MR,(int(cols/2), int(rows/2)))
+                crop = frameLeft[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]        
+                name = "face" + str(global_counter) + ".jpg"
+                print(name)
+                cv2.imwrite(name, crop)
+                image = {'photo':open(name, 'rb')}
+                #requests.post(URL, files = image)
+                global_counter += 1
+
+            for i, face_rect in enumerate(detected_right):
+                frameRight = cv2.warpAffine(frame,MR,(int(cols/2), int(rows/2)))
+                crop = frameRight[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]
+                
+                name = "face" + str(global_counter) + ".jpg"
+                cv2.imwrite(name, crop)
+                print(name)
+                image = {'photo':open(name, 'rb')}
+                #requests.post(URL, files = image)
+                global_counter += 1
+                
+            for i, face_rect in enumerate(detected_faces):
+                frameUp = cv2.warpAffine(frame, MU,(int(cols/2), int(rows/2)))
+                crop = frameUp[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]
+                name = "face" + str(global_counter) + ".jpg"
+                cv2.imwrite(name, crop)
+                print(name)
+                image = {'photo':open(name, 'rb')}
+                #requests.post(URL, files = image)
+                global_counter += 1
+                
+        if (counter == 2):
+            cv2.imwrite("frame.jpg", frame)
+            image = {'photo':open('frame.jpg', 'rb')}
+            #requests.post(URL, files = image)
             counter = 0
-        counter += 1
-
             
-        #crop = frame[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]
-
+        print(counter)
+        counter += 1
         
-    stream.truncate()
-    stream.seek(0)
-    rawCapture.truncate(0)
+        stream.truncate()
+        stream.seek(0)
+        rawCapture.truncate(0)
 
 # Function commands/help/options
 def main():
