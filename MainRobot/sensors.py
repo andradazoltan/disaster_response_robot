870f8b16a740faf827bc2f620ac055379b072355
Andrada Zoltan <andrada.12321@gmail.com>
2018-04-03 19:14:15 -0700

    Updated fritzing

--- MainRobot/sensors.py
+++ MainRobot/sensors.py
@@ -9,8 +9,8 @@ GPIO.setmode(GPIO.BOARD)
 tempCh = 3
 lightCh = 2
 
-hsIN = 12
-hsOUT = 12
+infra = 12
+GPIO.setup(infra,GPIO.OUT)
 
 servo = 7
 GPIO.setup(servo, GPIO.OUT)
@@ -80,64 +80,10 @@ def turnServo(angle):
     servoPWM.ChangeDutyCycle(0)
 
 
-""" Initializes communiction with DHT11 sensor and
-    reads in data from the sensor. Returns air humidity
-    as a percentage."""
-def readHumidity():
-    GPIO.setup(hsOUT, GPIO.OUT)
-    #set data line to high to indicate start signal
-    GPIO.output(hsOUT, True)
-    time.sleep(0.25)
-
-    #set data line low for 20micro s
-    GPIO.output(hsOUT, False)
-    time.sleep(0.02)
-
-    #end start signal by setting data line high for 40micro s
-    GPIO.output(hsOUT, True)
-    time.sleep(0.00004)
-
-    #start reading data line
-    GPIO.setup(hsIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    time.sleep(0.00001)
-   
-    #first: expect a low response from sensor for 80micro s
-    while GPIO.input(hsIN) != 0:
-            time.sleep(0.0000001)
-    time.sleep(0.00008)
-    
-    #second: expect a high response from sensor for 80micro s
-    while GPIO.input(hsIN) != 1:
-        time.sleep(0.0000001)
-    time.sleep(0.00008)
-    
-    #start to read incoming data, data sent as such:
-        #50micro s
-        #if 30micro s HIGH pulse, data = 0
-        #else, data = 1
-    times = [None]*80
-    data = [None]*5
-    for i in range(0,80):
-        times[i] = (getMicroSeconds(0))
-        times[i+1] = (getMicroSeconds(1))
-   
-    #parse through read times to see what data bits were sent
-    for i in range(0,38):
-        lowtime = times[2*i]
-        hightime = times[2*i+1]
-
-        data[i/8] <<= 1
-
-        if hightime > lowtime:
-            data[i/8] |= 1
-    return data[0]
-
-""" Returns the amount of microseconds the humidity
-    sensor is outputting a certain signal level."""
-def getMicroSeconds(level):
-    count = 0
-    while GPIO.input(hsIN) == level:
-        count += 1
-        time.sleep(0.00001)
-        
-    return count
+""" Turns on infrared LED
+"""
+def onInfrared():
+    if readLight < 60:
+	GPIO.output(infra, True)
+    else
+	GPIO.output(infra, False)
--- Motors/motortest.py
+++
@@ -1,3 +0,0 @@
-import motor
-
-straight(255)
