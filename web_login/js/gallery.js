85823af66e6fbbb2f66345aa4a8427bdc82c59a7
Cynthia <cynthiazhou98@gmail.com>
2018-04-03 23:56:07 -0700

    Update gallery.js

--- web_login/js/gallery.js
+++ web_login/js/gallery.js
@@ -6,7 +6,55 @@ window.onload = setupGallery();
 // function getPhotos(){
 //     $.ajax({
 //         type: 'GET',
-//         url: 'https://jsonplaceholder.typicode.com/posts',
+//         url: 'https://jsvar photoArr;
+
+window.onload = getPhotos();
+
+function getPhotos(){
+    $.ajax({
+        type: 'GET',
+        url: 'http://38.88.75.83/db/robotpictures.php',
+        success: function(data) {
+            console.log("sucessfully got photos");
+            parseData(JSON.parse(data));
+        },
+        error: function(data) {
+            console.log("error getting data");
+        },
+    });
+}
+
+function parseData(data){
+    photoArr = data;
+    setupGallery();
+}
+
+function setupGallery(){
+
+    for (var i = 0; i < photoArr.length; i++) {
+        var div = document.querySelector('.container-login100');
+
+        var imageDiv = document.createElement('div');
+        imageDiv.className = "wrap-login100 m-t-15 m-b-15 p-l-55 p-r-55 p-t-40 p-b-30";
+
+        var img = document.createElement('img');
+        img.className = 'list-img';
+        img.src = photoArr[i];
+
+        var title = document.createElement('div');
+        title.className = 'home-page-label m-t-15';
+        title.textContent = "Status: ";
+        var span = document.createElement('span');
+        span.className = "home-page-text";
+        span.textContent += "Unmatched face";
+
+        title.appendChild(span);
+        imageDiv.appendChild(img);
+        imageDiv.appendChild(title);
+        div.appendChild(imageDiv);
+    }
+}
+onplaceholder.typicode.com/posts',
 //         data: {
 //             format: 'json'
 //         },
